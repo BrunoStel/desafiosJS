eaf455b8559844b19bc7557fe19e66a7d771cc28
Bruno Stelmastchuk <brunostel@gmail.com>
2021-09-29 17:50:31 -0300

    Adicionando comentarios ao desafio 5

--- desafio5.js
+++ desafio5.js
@@ -2,17 +2,16 @@
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 
-
 function spinalCase(str) {
 
-    //  \s  => //whitespace    |_|  => _      (?=[A-Z])  => Se após aquela string possui uma letra Maísucula
+    //  \s  => //whitespace    _  => _      (?=[A-Z])  => Se s string que vem logo após é uma letra Maísucula. Sendo | => ou
     
-    const regExp = /\s|_|(?=[A-Z])/
+    const regExp = /_|\s|(?=[A-Z])/
     
       return str
-        .split(regExp)
-        .join("-")
-        .toLowerCase()
+        .split(regExp) //Converte a string em um array, utilizando como separador o argumento passado que é uma expressao regular
+        .join("-") //Juntando o array separando os índices por -, passado como argumento de join
+        .toLowerCase() //Transforma toda a String em caixa baixa
     
     }
     
